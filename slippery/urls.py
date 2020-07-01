6ef3537bc3f64da0f1ce699a11f3835284354d47
C. H. Lay <development@cschlay.com>
2020-07-01 19:09:32 +0300

    Add logout

--- slippery/urls.py
+++ slippery/urls.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import logout
 from django.urls import path, include
+
+def logout_view(request):
+    logout(request)
+
 
 urlpatterns = [
     path('', include('slippery.core.urls')),
     path('', include('django.contrib.auth.urls')),
+    path('logout/', logout_view, name="logout")
 ]

ad3d6f151a777f20dff68c76ae181ddb290d6d08
C. H. Lay <development@cschlay.com>
2020-07-02 19:46:34 +0300

    Add logging to projects

--- slippery/projects/models.py
+++ slippery/projects/models.py
@@ -5,4 +5,18 @@ class Project(models.Model):
     title = models.CharField(max_length=255)
     git_repository = models.URLField(max_length=255)
     repository_access_key = models.TextField(max_length=2000, blank=True, null=True)
-    project_domain = models.CharField(max_length=100)
+    domain = models.CharField(max_length=100)
+
+
+class ProjectLog(models.Model):
+    LEVEL_INFO = 'info'
+    LEVEL_ERROR = 'error'
+    LEVEL_CHOICES = [
+        (LEVEL_INFO, 'info'),
+        (LEVEL_ERROR, 'error')
+    ]
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    logged_at = models.DateTimeField(auto_now_add=True)
+    level = models.CharField(max_length=10, choices=LEVEL_CHOICES)
+    text = models.CharField(max_length=100)
--- slippery/projects/views.py
+++ slippery/projects/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, DeleteView
 
-from slippery.projects.models import Project
+from slippery.projects.models import Project, ProjectLog
 
 
 class ProjectCreateView(LoginRequiredMixin, CreateView):
@@ -14,7 +14,11 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        instance = self.object
+        ProjectLog.objects.create(
+            project=self.object,
+            level=ProjectLog.LEVEL_INFO,
+            text='Project created.'
+        )
         # Deploy the project.
         return response
 
@@ -22,6 +26,11 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
 class ProjectDetailView(DetailView):
     template_name = 'projects/project-detail.html'
     model = Project
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['logs'] = ProjectLog.objects.filter(project=self.object)
+        return context
 
     def get_queryset(self):
         return Project.objects.all()

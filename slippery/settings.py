5086157172f3f3416c7fdc796f4d9546960555b4
C. H. Lay <development@cschlay.com>
2020-07-04 14:16:58 +0300

    Add logging

--- slippery/settings.py
+++ slippery/settings.py
@@ -87,6 +87,24 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/logs/slippery.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
